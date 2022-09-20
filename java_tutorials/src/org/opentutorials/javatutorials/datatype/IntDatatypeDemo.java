2c8e072e8b36e96aaaf2e495e4158248c837b622
jo-dong <ehdgus4498@naver.com>
2022-09-20 14:13:58 +0900

    java_eclipse1

---
+++ java_tutorials/src/org/opentutorials/javatutorials/datatype/IntDatatypeDemo.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.datatype;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		byte a;		// byte의 표현 가능 범위 : -128 ~ 127
+		a = 128;	// a = 128; → Error
+	}
+
+}
---
+++ java_tutorials/src/org/opentutorials/javatutorials/variables/DoubleDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.variables;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a;	// double로 선언하면 a의 값 = 실수(Real Number)
+		a = 1.1;	// int a = 1.1; 은 Error 발생
+		
+		System.out.println(a + 1.1);
+	}
+
+}
